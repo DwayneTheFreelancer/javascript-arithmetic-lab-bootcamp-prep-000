bfbd77959bf7e894a2475983ebb65ec38599ab3a
Dwayne Munro <dwaynemunro1845@gmail.com>
2018-08-20 20:17:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,9 +25,13 @@ function dec(n) {
 }
 
 function makeInt(n) {
+<<<<<<< HEAD
   return parseInt(n, 10);
 }
 
 function preserveDecimal(n) {
   return parseFloat(n);
+=======
+  return parseInt(n);
+>>>>>>> 1faaa43da0e1c0d867725c4b5e2b776f934ca40c
 }
